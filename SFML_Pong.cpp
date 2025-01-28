76890c94a1d6372c804b4703ae3458a9d7bc6635
Travis Mix <travismix1980@gmail.com>
2025-01-28 09:39:02 -0800

    Add project files.

---
+++ SFML_Pong.cpp
@@ -0,0 +1,10 @@
+// SFML_Pong.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// by Travis Mix
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
